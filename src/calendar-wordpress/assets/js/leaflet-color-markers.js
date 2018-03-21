a9f4ff9132359993c9ff368a6cfc6b798cc60a58
bartn <bartn@DESKTOP-QSJR3ML>
2018-03-21 18:37:59 +0100

    Poprawki biblioteki Leaflet

---
+++ src/calendar-wordpress/assets/js/leaflet-color-markers.js
@@ -0,0 +1,71 @@
+var blueIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-blue.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-red.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-green.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-orange.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-yellow.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-violet.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-grey.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-icon-2x-black.png',
+	shadowUrl: '/wp-content/plugins/naprawiamy/assets/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
